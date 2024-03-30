376672f6e913a88c49ebd3c5ec84917431259eae
Bonekazz <juracirdantas@gmail.com>
2024-03-30 15:42:04 -0300

    inital commit

---
+++ backend/prisma/migrations/20240330183702_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "coins" INTEGER NOT NULL,
+    "tickets" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
