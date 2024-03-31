fbcf5ddd199e02fe85c7f09a453f2f9fde35a7e3
Bonekazz <juracirdantas@gmail.com>
2024-03-31 16:02:39 -0300

    feat > init api with user controller

---
+++ backend/src/api.ts
@@ -0,0 +1,28 @@
+import Express  from "express";
+import userController from "./modules/user/user.controller";
+
+const api = Express();
+api.use(Express.json());
+
+api.get('/api', (request, response) => {
+    return response.send({message: "good response"});
+});
+
+// api.post("api/login") login com autenticação
+
+// api.get("api/users") lista todos os users
+api.post("/api/users", (request, response) => userController.createUser(request, response)); // criar user
+
+// api.get("api/rooms") lista todas as salas
+
+// api.get("api/room/:roomId"){     //Para entrar em uma sala
+    // TODO: verificar se o usuario tem permissao para estar na rommId
+
+    // TODO: verificar se o usuario tem saldo suficiente para fazer a aposta
+//} 
+
+// api.get("api/bets") + query params para filtros
+
+api.listen(3000, () => {
+    console.log("server running on: http://localhost:3000");
+});
