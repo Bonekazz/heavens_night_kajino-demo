f1b3380c33e9515b536872c4b2ba9f8e5a4d688f
Bonekazz <juracirdantas@gmail.com>
2024-03-31 16:03:30 -0300

    feat > create prisma service with create function

---
+++ backend/src/modules/user/user.service.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { userDTO } from "./user.dto";
+
+export class UserService {
+    constructor(private prisma: PrismaClient) {}
+
+    async create(data: userDTO) {
+        const userExist = await this.prisma.user.findUnique({
+            where: {
+                name: data.name
+            }
+        });
+
+        if(userExist) return {message: "this username is already taken!"};
+
+        const newUser = await this.prisma.user.create({
+            data: {
+                id: data.id,
+                name: data.name,
+                pass: data.pass,
+                coins: data.coins,
+                tickets: data.tickets,
+                createdAt: undefined,
+            },
+        });
+
+        await this.prisma.$disconnect();
+
+        return newUser;
+    }
+}
