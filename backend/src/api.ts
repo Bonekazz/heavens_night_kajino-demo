5d9c73621825d2cb911055945ff6b6076d3952fd
Bonekazz <juracirdantas@gmail.com>
2024-04-02 15:35:13 -0300

    feat > create claim bonus route

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -3,6 +3,7 @@ import userController from "./modules/user/user.controller";
 import authController from "./modules/auth/auth.controller";
 import { AuthMiddleware } from "./modules/auth/auth.middleware";
 import roomController from "./modules/room/room.controller";
+import bonusController from "./modules/bonus/bonus.controller";
 
 const api = Express();
 api.use(Express.json());
@@ -18,18 +19,33 @@ api.post("/api/users",
 api.get("/api/rooms", 
     (request, response, next) => AuthMiddleware(request, response, next), 
     (request, response) => roomController.getList(request, response)
-);    
-api.get("/api/rooms/:roomId");
+); 
+
+// api.get(¬/api/bonus") // to check if the bonus is available
+api.post("/api/claimBonus", 
+    (request, response, next) => AuthMiddleware(request, response, next),
+    (request, response) => bonusController.claimBonus(request, response)
+);
+
+
+//api.get("/api/rooms/:roomId");
 // api.get("/api/rooms/:roomId?params..");
 
-// api.get("api/room/:roomId"){     //Para entrar em uma sala
+// api.get("/api/room/:roomId"){     //Para entrar em uma sala
     // TODO: verificar se o usuario tem permissao para estar na rommId
 
     // TODO: verificar se o usuario tem saldo suficiente para fazer a aposta
 //} 
 
-// api.get("api/bets") + query params para filtros
+// api.post("/api/bets") 
+// api.get("/api/bets") + query params para filtros
 
 api.listen(3000, () => {
     console.log("server running on: http://localhost:3000");
+    // console.log("[SERVER]: Executing shecdules ...")
+    // try {
+        // shell.execute(cronjob)
+    // } catch (error) {
+        // console.log(`[ERROR]: ${error}`)
+    // }
 });
