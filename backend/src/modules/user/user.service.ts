6c2f206292f7d4210cb4c5d3977f4c63dc7aa24f
Bonekazz <juracirdantas@gmail.com>
2024-04-01 22:51:42 -0300

    feat > add password encryption and token assign

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,31 +1,35 @@
 import { PrismaClient } from "@prisma/client";
-import { userDTO } from "./user.dto";
+import { hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 export class UserService {
     constructor(private prisma: PrismaClient) {}
 
-    async create(data: userDTO) {
+    async create({id, name, pass, coins, tickets}) {
         const userExist = await this.prisma.user.findUnique({
             where: {
-                name: data.name
+                name: name
             }
         });
 
         if(userExist) return {message: "this username is already taken!"};
 
-        const newUser = await this.prisma.user.create({
+        const hashedpass = await hash(pass, 8);
+
+        await this.prisma.user.create({
             data: {
-                id: data.id,
-                name: data.name,
-                pass: data.pass,
-                coins: data.coins,
-                tickets: data.tickets,
+                id: id,
+                name: name,
+                pass: hashedpass,
+                coins: coins,
+                tickets: tickets,
                 createdAt: undefined,
             },
         });
 
         await this.prisma.$disconnect();
 
-        return {message: "user created with sucess!"};
+        const token = sign({id: id}, "secret-key", {expiresIn: "1d"});
+        return {message: "user created with sucess!", data: {username: name, token: token}};
     }
 }
