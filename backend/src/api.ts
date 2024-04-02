f285414d5af179d6fc6dccc454d7e9ef9c4b1446
Bonekazz <juracirdantas@gmail.com>
2024-04-02 10:56:19 -0300

    feat > create rooms route handler

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,20 +1,26 @@
 import Express  from "express";
 import userController from "./modules/user/user.controller";
 import authController from "./modules/auth/auth.controller";
+import { AuthMiddleware } from "./modules/auth/auth.middleware";
+import roomController from "./modules/room/room.controller";
 
 const api = Express();
 api.use(Express.json());
 
-api.get('/api', (request, response) => {
-    return response.send({message: "good response"});
-});
+api.post("/api/login", 
+    (request, response) => authController.login(request, response)
+);
 
-api.post("/api/login", (request, response) => authController.login(request, response));
+api.post("/api/users", 
+    (request, response) => userController.createUser(request, response)
+    ); // criar user
 
-// api.get("api/users") lista todos os users
-api.post("/api/users", (request, response) => userController.createUser(request, response)); // criar user
-
-// api.get("api/rooms") lista todas as salas
+api.get("/api/rooms", 
+    (request, response, next) => AuthMiddleware(request, response, next), 
+    (request, response) => roomController.getList(request, response)
+);    
+api.get("/api/rooms/:roomId");
+// api.get("/api/rooms/:roomId?params..");
 
 // api.get("api/room/:roomId"){     //Para entrar em uma sala
     // TODO: verificar se o usuario tem permissao para estar na rommId
