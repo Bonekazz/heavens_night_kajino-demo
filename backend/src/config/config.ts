1a21ee4480f76ee3b3039edb30cc08205b573d14
Bonekazz <juracirdantas@gmail.com>
2024-04-03 15:36:46 -0300

    feat > add cors configuration

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,1 +1,8 @@
 export const SECRET_KEY = "desafio_programador_python";
+
+export const CORS_CONFIG = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
