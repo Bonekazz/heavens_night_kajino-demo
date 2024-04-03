57bdbd8aea8228fb1eb3060942313ddebd7d984b
Bonekazz <juracirdantas@gmail.com>
2024-04-03 15:41:53 -0300

    feat > create colored logs

--- backend/src/utils/colored_logs.ts
+++ backend/src/utils/colored_logs.ts
@@ -1,5 +1,14 @@
 import logger from "node-color-log";
 
-export default function serverlog(text: string) {
+
+export function serverlog(text: string) {
     logger.color("white").bgColor("green").bold().log(`[SERVER]: ${text}`);
+}
+
+export function errorlog(text: string){
+    logger.color("white").bgColor("red").bold().log(`[SERVER-ERROR]: ${text}`);
+}
+
+export function schedlog(text: string){
+    logger.color("white").bgColor("blue").bold().log(`[schedule]: ${text}`);
 }
