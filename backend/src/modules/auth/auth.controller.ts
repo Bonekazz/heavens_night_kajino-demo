6ac4f939bae312a026d3aad9e16acdda5b14af82
Bonekazz <juracirdantas@gmail.com>
2024-04-03 15:38:38 -0300

    refactor > handle service response and return token cookie's response

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -9,7 +9,17 @@ class AuthController {
 
         const {username, password} = request.body;
 
-        return response.json(await this.authService.authenticate({username, password}));
+        if(!username || !password) return response.json({error: "parametros invalidos (undefined)"});
+
+        const serviceRes = await this.authService.authenticate({username, password});
+
+        if(!serviceRes.sucess) return response.status(401).json(serviceRes);
+
+        console.log("cookie adicionado ao client");
+        return response
+                .status(200)
+                .cookie("heavensNightKajinoToken", serviceRes.token, {httpOnly: true, sameSite: "none", secure: true})
+                .json({success: serviceRes, message: serviceRes.message});
         
     }
 }
